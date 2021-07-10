ac93e9d2f61026c440244b73d77d0e27977b50b0
chiWorld <fellyoul7@gmail.com>
2021-07-11 00:07:30 +0900

    commit on 'master'

--- src/App.js
+++ src/App.js
@@ -9,28 +9,27 @@ function Food({ name, picture }) {
   );
 }
 
-function renderFood(dish) {
-  return <Food name={dish.name} picture={dish.image} />;
-}
-
 function App() {
   return (
     <div>
-        {foodILike.map(renderFood)}
+        {foodILike.map(dish => (<Food key={dish.id} name={dish.name} picture={dish.image} />))}
     </div>
   );
 }
 
 const foodILike = [
   {
+    id: 1,
     name : 'Kimchi',
     image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.-xNPwouDqZDynzHSu2ctSAHaFC%26pid%3DApi&f=1',
   },
   {
+    id: 2,
     name : 'Samgyeopsal',
     image : 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.JdMkDRS5dlMNJqMBr8vWDQHaFj%26pid%3DApi&f=1',
   },
   {
+    id: 3,
     name : 'Bibimbap',
     image : 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.z9ft1sb7e-dWAAnUh7igAAHaFA%26pid%3DApi&f=1',
   },
