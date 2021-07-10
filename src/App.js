1eb1b7e931f943f404db139ec4a707abbe639481
chiWorld <fellyoul7@gmail.com>
2021-07-10 23:14:06 +0900

    commit on 'master'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h2>I like {name} </h2>
+      <img src={picture} alt="profile" />
+    </div>
+  );
+}
+
+const foodILike = [
+  {
+    name : 'Kimchi',
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.-xNPwouDqZDynzHSu2ctSAHaFC%26pid%3DApi&f=1',
+  },
+  {
+    name : 'Samgyeopsal',
+    image : 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.JdMkDRS5dlMNJqMBr8vWDQHaFj%26pid%3DApi&f=1',
+  },
+  {
+    name : 'Bibimbap',
+    image : 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.z9ft1sb7e-dWAAnUh7igAAHaFA%26pid%3DApi&f=1',
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        {foodILike.map(dish => (
+         <Food name={dish.name} picture={dish.image} />
+        ))}
     </div>
   );
 }
